fe6634d478cf1075083fd64e234e0cb0bc2849a3
Wahid Khan <wahidzk0091@gmail.com>
2023-11-24 12:34:40 +0000

    Kernel: Rebrand to NoVA
    also create a build.sh script
    build-ci: Create new workflow
    credits to @itsHanibee for script ref
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- build.sh
+++ build.sh
@@ -1,50 +1,112 @@
 #!/bin/bash
+#
+# Compile script for NoVA Kernel.
+# Copyright (C)2022 Ardany Jolón
+# Credits to @ItsHaniBee
 
-function compile() 
-{
+SECONDS=0 # builtin bash timer
+KERNEL_PATH=$PWD
+AK3_DIR="$HOME/tc/Anykernel"
+DEFCONFIG="pissarro_user_defconfig"
+export KBUILD_BUILD_USER=Abdul7852
+export KBUILD_BUILD_HOST=NoVA
 
-source ~/.bashrc && source ~/.profile
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
-export ARCH=arm64
-export KBUILD_BUILD_HOST=MARKxDEVS
-export KBUILD_BUILD_USER="AbzRaider"
-git clone --depth=1  https://gitlab.com/LeCmnGend/proton-clang.git -b clang-13  clang
+# Install needed tools
 
+if [[ $1 = "-t" || $1 = "--tools" ]]; then
+        mkdir toolchain
+        cd toolchain
 
+        curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman" || exit 1
 
- if ! [ -d "out" ]; then
-echo "Kernel OUT Directory Not Found . Making Again"
-mkdir out
+        chmod -x antman
+
+        echo 'Setting up toolchain in $(PWD)/toolchain'
+        bash antman -S || exit 1
+
+        echo 'Build libarchive for bsdtar'
+        git clone https://github.com/libarchive/libarchive || true
+        cd libarchive
+        bash build/autogen.sh
+        ./configure
+        make -j$(nproc)
+        cd ..
+
+        echo 'Patch for glibc'
+        wget https://gist.githubusercontent.com/itsHanibee/fac63ea2fc0eca7b8d7dcbb7eb678c3b/raw/beacf8f0f71f4e8231eaa36c3e03d2bee9ae3758/patch-for-old-glibc.sh
+        export PATH=$(pwd)/libarchive:$PATH
+        bash patch-for-old-glibc.sh
 fi
 
-make O=out ARCH=arm64 pissarro_user_defconfig
+# Regenerate defconfig file
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+        make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+        cp out/defconfig arch/arm64/configs/$DEFCONFIG
+        echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+fi
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
-		      LD=ld.lld \
-                      STRIP=llvm-strip \
-                      AS=llvm-as \
-		      AR=llvm-ar \
-		      NM=llvm-nm \
-		      OBJCOPY=llvm-objcopy \
-   		      OBJDUMP=llvm-objdump \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
-}
+if [[ $1 = "-b" || $1 = "--build" ]]; then
+        export ARCH=arm64
+        PATH=$PWD/toolchain/bin:$PATH
+        export USE_HOST_LEX=yes
+        mkdir -p out
+        make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 $DEFCONFIG
+        exec 2> >(tee -a out/error.log >&2)
+        echo -e ""
+        echo -e ""
+        echo -e "*****************************"
+        echo -e "**                         **"
+        echo -e "** Starting compilation... **"
+        echo -e "**                         **"
+        echo -e "*****************************"
+        echo -e ""
+        echo -e ""
+        make O=out CC=clang LLVM=1 LLVM_IAS=1 AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- -j$(nproc) || exit 1
 
-function zupload()
-{
-rm -rf AnyKernel	
-git clone --depth=1 https://github.com/AbzRaider/AnyKernel33 -b pissarro AnyKernel
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-cd AnyKernel
-zip -r9 Test-OSS-KERNEL-PISSARRO-T.zip *
-curl --upload-file "Test-OSS-KERNEL-PISSARRO-T.zip" https://free.keep.sh
-}
-compile
-zupload
+        kernel="out/arch/arm64/boot/Image.gz-dtb"
+
+        if [ -f "$kernel" ]; then
+                rm *.zip 2>/dev/null
+                # Set kernel name and version
+                SUBREV="4.14.$(cat "Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')"
+                REVISION=NoVA-Pissarro
+                ZIPNAME=""$REVISION"-"$SUBREV".zip"
+                echo -e ""
+                echo -e ""
+                echo -e "********************************************"
+                echo -e "\nKernel compiled succesfully! Zipping up...\n"
+                echo -e "********************************************"
+                echo -e ""
+                echo -e ""
+        if [ -d "$AK3_DIR" ]; then
+                cp -r $AK3_DIR Anykernel
+        elif ! git clone -q https://github.com/AbzRaider/AnyKernel33 -b pissarro Anykernel; then
+                        echo -e "\nAnyKernel repo not found locally and couldn't clone from GitHub! Aborting..."
+        fi
+                cp $kernel Anykernel
+                rm -rf out/arch/arm64/boot
+                cd Anykernel
+                git checkout master &> /dev/null
+                zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+                cd ..
+
+        echo -e ""
+        echo -e ""
+        echo -e "************************************************************"
+        echo -e "**                                                        **"
+        echo -e "**   File name: $ZIPNAME   **"
+        echo -e "**   Build completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!    **"
+        echo -e "**                                                        **"
+        echo -e "************************************************************"
+        echo -e ""
+        echo -e ""
+        else
+        echo -e ""
+        echo -e ""
+        echo -e "*****************************"
+        echo -e "**                         **"
+        echo -e "**   Compilation failed!   **"
+        echo -e "**                         **"
+        echo -e "*****************************"
+        fi
+        fi
