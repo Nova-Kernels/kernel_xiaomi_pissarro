36e857aabe115b8332dec4118359fb3d8ee30431
Juhyung Park <qkrwngud825@gmail.com>
2024-04-23 16:57:21 +0000

    BACKPORT: sched: promote nodes out of CONFIG_SCHED_DEBUG
    - This patch was backported from [1].
    - the previous commit in [2] was incomplete so pick this over it for properly promoting nodes
    
    [1]: arter97/android_kernel_oneplus_sm8350@0ed5b00
    [2] https://github.com/Nova-Kernels/kernel_xiaomi_mt6785/commit/4ccfb17b94192d95dcb7628f24a987056833b966
    [cyberknight777: backport to 4.14]
    [Wahid7852: also re-order the struct table while at it]
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -343,24 +343,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-	{
-		.procname	= "sched_min_granularity_ns",
-		.data		= &sysctl_sched_min_granularity,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= sched_proc_update_handler,
-		.extra1		= &min_sched_granularity_ns,
-		.extra2		= &max_sched_granularity_ns,
-	},
-	{
-		.procname	= "sched_latency_ns",
-		.data		= &sysctl_sched_latency,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= sched_proc_update_handler,
-		.extra1		= &min_sched_granularity_ns,
-		.extra2		= &max_sched_granularity_ns,
-	},
 #ifdef CONFIG_MTK_SCHED_BOOST
 	{
 		.procname       = "sched_isolation_hint",
@@ -400,6 +382,24 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 #endif
+	{
+		.procname	= "sched_min_granularity_ns",
+		.data		= &sysctl_sched_min_granularity,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_proc_update_handler,
+		.extra1		= &min_sched_granularity_ns,
+		.extra2		= &max_sched_granularity_ns,
+	},
+	{
+		.procname	= "sched_latency_ns",
+		.data		= &sysctl_sched_latency,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_proc_update_handler,
+		.extra1		= &min_sched_granularity_ns,
+		.extra2		= &max_sched_granularity_ns,
+	},
 	{
 		.procname	= "sched_sync_hint_enable",
 		.data		= &sysctl_sched_sync_hint_enable,
